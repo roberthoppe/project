eb9dd0fc44d1ead1e4f150250578b37ef26e3594
roberthoppe <robert_hoppe@interia.eu>
2019-01-30 20:01:55 +0100

    1

--- src/main/java/pl/sda/gda15/project/model/UserPOJO.java
+++ src/main/java/pl/sda/gda15/project/model/UserPOJO.java
@@ -1,4 +1,17 @@
 package pl.sda.gda15.project.model;
 
+import java.util.UUID;
+
 public class UserPOJO {
+
+    private UUID userId;
+
+    private String name;
+    private String surname;
+    private String adres;
+    private String city;
+    private String country;
+    private String email;
+    private String tel;
+
 }
