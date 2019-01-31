4c846f380ce73ebcfd5fc8405cd33ff89a0cb55e
roberthoppe <robert_hoppe@interia.eu>
2019-02-01 00:55:27 +0100

    Add Products

--- src/main/java/pl/sda/gda15/project/controler/ProductController.java
+++ src/main/java/pl/sda/gda15/project/controler/ProductController.java
@@ -2,6 +2,9 @@ package pl.sda.gda15.project.controler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.gda15.project.model.ProductPOJO;
 import pl.sda.gda15.project.service.ProductService;
 
 @Controller
@@ -11,5 +14,15 @@ private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+    @GetMapping
+    public String product(Model model){
+        model.addAttribute("product",new ProductPOJO());
+        return "product";
+    }
+    @GetMapping("/products")
+    public String products(Model model){
+        model.addAttribute("roducts",productService.allProducts());
+        return "products";
     }
 }
--- src/main/java/pl/sda/gda15/project/service/ProductService.java
+++ src/main/java/pl/sda/gda15/project/service/ProductService.java
@@ -6,12 +6,19 @@ import pl.sda.gda15.project.domain.entity.Product;
 import pl.sda.gda15.project.domain.repository.ProductRepository;
 import pl.sda.gda15.project.model.ProductPOJO;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
 @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+
+    }
+    public List<ProductPOJO> allProducts(){
+    return productRepository.findAll().stream().map(this::map).collect(Collectors.toList());
     }
     public Product map(ProductPOJO productPOJO){
     return new Product(productPOJO.getProductId(),productPOJO.getProductName(),productPOJO.getQuantity());
